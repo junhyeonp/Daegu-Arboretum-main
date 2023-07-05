b45a5fac207dd57e1dde4901bbd937847c118d5a
junhyeonp <bjune0307@gmail.com>
2023-07-05 10:45:32 +0900

    commit

--- js/kakao-map.js
+++ js/kakao-map.js
@@ -20,5 +20,5 @@ var marker = new kakao.maps.Marker({
 // 마커가 지도 위에 표시되도록 설정합니다
 marker.setMap(map);
 
-// 아래 코드는 지도 위의 마커를 제거하는 코드입니다
-// marker.setMap(null);
+  // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+  // marker.setMap(null);
