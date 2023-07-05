ad8780d2abc28c26a16aec9d176988dfe83ec855
junhyeonp <bjune0307@gmail.com>
2023-07-05 10:55:08 +0900

    commit

--- js/kakao-map.js
+++ js/kakao-map.js
@@ -1,24 +1,27 @@
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-  mapOption = {
-    center: new kakao.maps.LatLng(35.79893470858549, 128.52342202498397), // 지도의 중심좌표
-    level: 4, // 지도의 확대 레벨
-  };
+if (kakao.maps) {
 
-var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+  var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+      center: new kakao.maps.LatLng(35.79893470858549, 128.52342202498397), // 지도의 중심좌표
+      level: 4, // 지도의 확대 레벨
+    };
 
-// 마커가 표시될 위치입니다
-var markerPosition = new kakao.maps.LatLng(
-  35.79893470858549,
-  128.52342202498397
-);
+  var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-// 마커를 생성합니다
-var marker = new kakao.maps.Marker({
-  position: markerPosition,
-});
+  // 마커가 표시될 위치입니다
+  var markerPosition = new kakao.maps.LatLng(
+    35.79893470858549,
+    128.52342202498397
+  );
 
-// 마커가 지도 위에 표시되도록 설정합니다
-marker.setMap(map);
+  // 마커를 생성합니다
+  var marker = new kakao.maps.Marker({
+    position: markerPosition,
+  });
+
+  // 마커가 지도 위에 표시되도록 설정합니다
+  marker.setMap(map);
 
   // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
   // marker.setMap(null);
+}
